84e1a6d7974838bc8d8d90ef580bd9c6098d1859
Talha Mirza <mirzatalha178@gmail.com>
2021-01-28 02:07:52 +0500

    add projects to Counter.jsx

--- src/components/Counter.jsx
+++ src/components/Counter.jsx
@@ -8,21 +8,22 @@ class Counter extends Component
 {
         render() {         
         return (    
-                 <div className="row">
+           <div className="row">
                  
-                 <div className="col-1">
+          <div className="col-1">
                  <span className={this.getBadgeClasses()}>{this.formatCount()}</span>
-                 </div>                 
-                 <div className="col-1">
+       </div>                 
+                 <div className="col-2">
                    <button 
                   onClick={() => this.props.onIncrement(this.props.counter)}
-                  className="btn btn-secondary btn-sm"
+                  className="btn btn-secondary btn-sm m-2"
                   >
                   +
                   </button>
                   <button 
                   onClick={() => this.props.onDecrement(this.props.counter)}
-                  className="btn btn-secondary btn-sm"
+                  className="btn btn-secondary btn-sm m-2"
+                   disabled={this.props.counter.value === 0 ? "disabled" : ""}
                   >
                   -
                   </button>
